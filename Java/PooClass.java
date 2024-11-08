a186ec263e2aa468c5be8cb9b6d61a51dcda103a
lm-gomes <lm-gomes>
2024-11-08 10:27:34 -0300

    exe4 monitlist

--- Java/PooClass.java
+++ Java/PooClass.java
@@ -1,8 +1,44 @@
 import java.util.Scanner;
 
 public class PooClass{
+	public double Calculator(double a, double b, int op){
+		switch(op){
+			//Addition
+			case 1:
+				return a + b;
+				
+			//Subtraction
+			case 2:
+				return a - b;
+				
+			//Multiplication
+			case 3:
+				return a * b;
+			
+			//Division
+			case 4:
+				return a / b;
+			
+			default:
+				System.out.println("Invalid operation!");
+		}
+		return 0;
+	}
+
 	public static void main(String[] args){
-		System.out.printf("- - - -CALCULATOR- - - -\n[1]Addition\n[2]Subtraction\n[3]Multiplication\n[4]Division\n>>");
+		PooClass obj = new PooClass();
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.printf("- - - -CALCULATOR- - - -\n[1]Addition\n[2]Subtraction		\n[3]Multiplication\n[4]Division\n>>");
+		
+		int op = scanner.nextInt();
+		double num = scanner.nextDouble();
+		double num1 = scanner.nextDouble();
+		double result = obj.Calculator(num, num1, op);
+		System.out.println("Result: " + result);
 		
 	}
+
+	
+
 }
