66ee481aae1a737e2693ecf8908bcbc938f1a59e
lm-gomes <lm-gomes>
2024-11-08 09:54:28 -0300

    first commit

---
+++ Java/PooClass.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class PooClass{
+	public static void main(String[] args){
+		System.out.printf("- - - -CALCULATOR- - - -\n[1]Addition\n[2]Subtraction\n[3]Multiplication\n[4]Division\n>>");
+		
+	}
+}
