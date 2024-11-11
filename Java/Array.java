eece941be836649ac723aefe5a04faf77c37236b
lm-gomes <lm-gomes>
2024-11-10 23:12:03 -0300

    arrayex

---
+++ Java/Array.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Array{
+	public static void main(String[] args){
+		Scanner scanner = new Scanner(System.in);		
+
+		int array[] = new int[5];
+		int sum = 0;
+
+		for(int i = 0; i < 5; i++){
+			System.out.printf("Digite o %d numero: ", i + 1);
+			array[i] = scanner.nextInt();
+		}
+		
+		for(int i = 0; i < 5; i++){
+			sum += array[i];			
+		}
+
+		System.out.printf("A soma e: %d", sum);		
+		
+	}
+}
